0fc0f8839c412ec154c880c5c7f9da997fa95594
MrSwoosh <151520698+MrSwoosh@users.noreply.github.com>
2026-03-22 14:46:53 +0100

    File rename

---
+++ Day_3.py
@@ -0,0 +1,54 @@
+"""
+Possible solution for Advent of Code 2016 Day 3
+https://adventofcode.com/2015/day/3
+
+Solving it all in 1 O(n) function
+"""
+
+import sys
+
+
+def main():
+    santa_part_1 = [0, 0]
+    santa_part_2 = [0, 0]
+    robo_santa = [0, 0]
+    seen_part_1 = {(0, 0)}
+    seen_part_2 = {(0, 0)}
+
+    direction = {'^': (1, 0),
+                 'v': (-1, 0),
+                 '<': (0, -1),
+                 '>': (0, 1)}
+
+    try:
+        with open("dataset_3.txt") as file:
+            instructions = file.read().strip()
+    except FileNotFoundError:
+        print("Something went wrong opening the input file.\n"
+              "Check it and try again.")
+        sys.exit(1)
+
+    for i, char in enumerate(instructions):
+        # Check validity of character
+        if char not in direction:
+            print("Illegal character found in instructions.\n"
+                  "Closing program.")
+            sys.exit(1)
+
+        current = santa_part_2 if i % 2 == 0 else robo_santa
+
+        dx, dy = direction[char]
+        santa_part_1[0] += dx
+        santa_part_1[1] += dy
+        current[0] += dx
+        current[1] += dy
+
+        seen_part_1.add(tuple(santa_part_1))
+        seen_part_2.add(tuple(current))
+
+    print(f"The number of houses that receive a present for part 1 is: {len(seen_part_1)}.")
+    print(f"The number of houses that receive a present for part 2 is: {len(seen_part_2)}.")
+
+
+if __name__ == "__main__":
+    main()
