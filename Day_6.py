474bfa7e6a1d8962656208c4b0927a4c6ab0f152
MrSwoosh <151520698+MrSwoosh@users.noreply.github.com>
2026-03-28 09:00:07 +0100

    Removed repeated casting, and added some more documentation

--- Day_6.py
+++ Day_6.py
@@ -10,19 +10,27 @@ Part 2
 
 The algorithm doesn't use an actual grid. It stores the x, y position
 as a tuple in a dictionary. This reduces memory usage by not storing
-values for unused positions.
+values for unused positions. Only efficient when not all positions are used.
+Instructions don't require searching in a grid, which could make a 2D grid
+a more efficient option. But this will require to manually search for grid
+limits in the input data, built in a method to determine size,
+ or resize grid during processing. I don't like either options,
+ making me prefer the dictionary approach.
 
 Time complexity
 gimme_data(): O(n)
 solve(): O(2n)
-_part_1(): O(n)
-_part_2(): O(n)
+_part_1(): for every instruction O(k) where k = surface of rectangle -> O(n * k) == O(n * grid size)
+_part_2(): for every instruction O(k) where k = surface of rectangle -> O(n * k) == O(n * grid size)
 
 Possible improvements
 -   _part_1() and _part_1() each iterate over the dataset.
-    Can be merged to 1 method to reduce iterations from 2 to 1.
+    Can be merged to 1 method to reduce iterations from 2 to 1,
+    but will diminish readability.
 -   Memory usage can be reduced further by removing a position
     from the dictionary when its value is 0.
+-   Number of dictionary look-up is high.
+    Could be reduced by using lights[(x, y)] = lights.get((x, y), 0) + 1
 """
 
 import sys
@@ -37,6 +45,7 @@ def gimme_data() -> list[str]:
         remove "through"
     Example:
         "toggle 461,550 through 564,900" -> "3 461,550 564,900"
+
     :return: List with transformed strings.
     """
     try:
@@ -53,6 +62,7 @@ def gimme_data() -> list[str]:
 def solve(data: list[str]) -> tuple[int, int]:
     """
     Handles solving each part individually.
+
     :param data: List with instructions. (list[str])
     :return: tuple with 2 ints, representing the solutions to part 1 and 2. (tuple[int, int])
     """
@@ -63,24 +73,34 @@ def solve(data: list[str]) -> tuple[int, int]:
 
 
 def _part_1(data: list[str]) -> int:
+    """
+    Processes instructions to find the solution to part 1.
+    The first character of every instruction sets the scenario.
+        1: turn on - set position state to 1, regardless of its state.
+        2: turn off - set position state to 0, regardless of its state.
+        3: toggle - set position state to 1 if current state is 0, or 0 if current state is 1.
+
+    :param data: List with instructions. (list[str])
+    :return: sum of lights that are on. (int)
+    """
     lights = dict()
 
     for instruction in data:
         scenario, start, end = instruction.split(" ")
-        x1, y1 = start.split(',')
-        x2, y2 = end.split(',')
+        x1, y1 = map(int, start.split(','))
+        x2, y2 = map(int, end.split(','))
         match scenario:
             case '1':
-                for x in range(int(x1), int(x2) + 1):
-                    for y in range(int(y1), int(y2) + 1):
+                for x in range(x1, x2 + 1):
+                    for y in range(y1, y2 + 1):
                         lights[(x, y)] = 1
             case '2':
-                for x in range(int(x1), int(x2) + 1):
-                    for y in range(int(y1), int(y2) + 1):
+                for x in range(x1, x2 + 1):
+                    for y in range(y1, y2 + 1):
                         lights[(x, y)] = 0
             case '3':
-                for x in range(int(x1), int(x2) + 1):
-                    for y in range(int(y1), int(y2) + 1):
+                for x in range(x1, x2 + 1):
+                    for y in range(y1, y2 + 1):
                         if (x, y) not in lights:
                             lights[(x, y)] = 1
                         else:
@@ -90,28 +110,39 @@ def _part_1(data: list[str]) -> int:
 
 
 def _part_2(data: list[str]) -> int:
+    """
+        Processes instructions to find the solution to part 2.
+        Uses the same logic as _part_1(), but with different values.
+        The first character of every instruction sets the scenario.
+            1: turn on - increase the value of the position by 1.
+            2: turn off - decrease the value of the position by 1. Lowest value is 0.
+            3: toggle - increase the value of the position by 2.
+
+        :param data: List with instructions. (list[str])
+        :return: sum of lights that are on. (int)
+        """
     lights_2 = dict()
 
     for instruction in data:
         scenario, start, end = instruction.split(" ")
-        x1, y1 = start.split(',')
-        x2, y2 = end.split(',')
+        x1, y1 = map(int, start.split(','))
+        x2, y2 = map(int, end.split(','))
         match scenario:
             case '1':
-                for x in range(int(x1), int(x2) + 1):
-                    for y in range(int(y1), int(y2) + 1):
+                for x in range(x1, x2 + 1):
+                    for y in range(y1, y2 + 1):
                         if (x, y) not in lights_2:
                             lights_2[(x, y)] = 0
                         lights_2[(x, y)] += 1
             case '2':
-                for x in range(int(x1), int(x2) + 1):
-                    for y in range(int(y1), int(y2) + 1):
+                for x in range(x1, x2 + 1):
+                    for y in range(y1, y2 + 1):
                         if (x, y) not in lights_2:
                             lights_2[(x, y)] = 0
                         lights_2[(x, y)] = max(0, lights_2[(x, y)] -1)
             case '3':
-                for x in range(int(x1), int(x2) + 1):
-                    for y in range(int(y1), int(y2) + 1):
+                for x in range(x1, x2 + 1):
+                    for y in range(y1, y2 + 1):
                         if (x, y) not in lights_2:
                             lights_2[(x, y)] = 2
                         else:
