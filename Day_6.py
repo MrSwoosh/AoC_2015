85b99569dd76926e0f83c50e4ac8e511dc2fcfc8
MrSwoosh <151520698+MrSwoosh@users.noreply.github.com>
2026-03-28 08:36:34 +0100

    Added some documentation

--- Day_6.py
+++ Day_6.py
@@ -1,10 +1,44 @@
 """
 Possible solution to Advent of Code 2015 Day 6.
 https://adventofcode.com/2015/day/6
+
+Imagine a 1000*1000 grid where every element represents a light.
+Part 1
+    Every light can be turned on, off or toggled.
+Part 2
+    Every light has a state representing its brightness.
+
+The algorithm doesn't use an actual grid. It stores the x, y position
+as a tuple in a dictionary. This reduces memory usage by not storing
+values for unused positions.
+
+Time complexity
+gimme_data(): O(n)
+solve(): O(2n)
+_part_1(): O(n)
+_part_2(): O(n)
+
+Possible improvements
+-   _part_1() and _part_1() each iterate over the dataset.
+    Can be merged to 1 method to reduce iterations from 2 to 1.
+-   Memory usage can be reduced further by removing a position
+    from the dictionary when its value is 0.
 """
+
 import sys
 
 def gimme_data() -> list[str]:
+    """
+    Retrieves data from a file, for the algorithm to process.
+    Transforms lines for easier processing.
+        "turn on" -> '1'
+        "turn off" -> '2'
+        "toggle" -> "3"
+        remove "through"
+    Example:
+        "toggle 461,550 through 564,900" -> "3 461,550 564,900"
+    :return: List with transformed strings.
+    """
     try:
         with open("dataset.txt") as file:
             return [line.strip().replace("through ", '').replace("turn on", '1')
@@ -17,6 +51,11 @@ def gimme_data() -> list[str]:
 
 
 def solve(data: list[str]) -> tuple[int, int]:
+    """
+    Handles solving each part individually.
+    :param data: List with instructions. (list[str])
+    :return: tuple with 2 ints, representing the solutions to part 1 and 2. (tuple[int, int])
+    """
     result_part1 = _part_1(data)
     result_part2 = _part_2(data)
 
